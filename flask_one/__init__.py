d07278bac94b356d1bfa753986f42125df8fc68b
giovanni <giovanni.herdigein@gmail.com>
2020-12-11 18:49:43 +0000

    Vroege aanpassingen

--- flask_one/__init__.py
+++ flask_one/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from .main.routes import main
+from .extensions import mongo
+import os
 
 app = Flask(__name__)
 app.register_blueprint(main)
-
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+mongo.init_app(app)
 if __name__=='__main__':
     app.run()
--- flask_one/main/routes.py
+++ flask_one/main/routes.py
@@ -1,8 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 import os
+from flask_one.extensions import mongo
+
 main = Blueprint('main',__name__)
 
 @main.route('/')
 def index():
+    todo_collection = mongo.db.todo
+    todos = todo_collection.find()
+
     print("hallo {}".format(os.environ['FLASK_RUN_PORT']))
-    return "Index"
+    return render_template('index.html',todos=todos)
